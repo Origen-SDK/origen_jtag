6174aa68dea364ebde8bf9aa59e3ac31cf46515a
Stephen McGinty <stephen.mcginty@freescale.com>
2015-08-07 08:45:52 -0500

    Added code coverage integration

--- Gemfile
+++ Gemfile
@@ -2,3 +2,11 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in origen_jtag.gemspec
 gemspec
+
+gem 'coveralls', require: false
+
+if RUBY_VERSION >= '2.0.0'
+  gem 'byebug', '~>3.5'
+else
+  gem 'debugger', '~>1.6'
+end
--- config/commands.rb
+++ config/commands.rb
@@ -1,11 +1,8 @@
 # This file should be used to extend the origen command line tool with tasks 
 # specific to your application.
-# The comments below should help to get started and you can also refer to
-# lib/origen/commands.rb in your Origen core workspace for more examples and 
-# inspiration.
 #
 # Also see the official docs on adding commands:
-#   http://origen.freescale.net/origen/latest/guides/custom/commands/
+#   http://origen-sdk.org/origen/guides/custom/commands/
 
 # Map any command aliases here, for example to allow origen -x to refer to a 
 # command called execute you would add a reference as shown below: 
@@ -17,65 +14,45 @@ aliases ={
 # the above alias table and should not be removed.
 @command = aliases[@command] || @command
 
-def path_to_coverage_report
-  require 'pathname'
-  Pathname.new("#{Origen.root}/coverage/index.html").relative_path_from(Pathname.pwd)
-end
-
-def enable_coverage(name, merge=true)
-  if ARGV.delete("-c") || ARGV.delete("--coverage")
-    require 'simplecov'
-    SimpleCov.start do
-      command_name name
-
-      at_exit do
-        SimpleCov.result.format!
-        puts ""
-        puts "To view coverage report:"
-        puts "  firefox #{path_to_coverage_report} &"
-        puts ""
-      end
-    end
-    yield
-  else
-    yield
-  end
-end
-
 # Now branch to the specific task code
 case @command
 
+## Run the unit tests  
+#when "specs"
+#  require "rspec"
+#  exit RSpec::Core::Runner.run(['spec'])
+
 when "examples"  
   Origen.load_application
-  enable_coverage("examples") do 
+  status = 0
 
-    # Pattern generator tests
-    ARGV = %w(jtag_workout -t v93k.rb -r approved)
-    load "#{Origen.top}/lib/origen/commands/generate.rb"
-    ARGV = %w(jtag_workout -t debug_RH1 -r approved)
-    load "#{Origen.top}/lib/origen/commands/generate.rb"
-    ARGV = %w(jtag_workout -t debug_RL1 -r approved)
-    load "#{Origen.top}/lib/origen/commands/generate.rb"
+  # Pattern generator tests
+  ARGV = %w(jtag_workout -t v93k.rb -r approved)
+  load "#{Origen.top}/lib/origen/commands/generate.rb"
+  ARGV = %w(jtag_workout -t debug_RH1 -r approved)
+  load "#{Origen.top}/lib/origen/commands/generate.rb"
+  ARGV = %w(jtag_workout -t debug_RL1 -r approved)
+  load "#{Origen.top}/lib/origen/commands/generate.rb"
 
-    ARGV = %w(jtag_workout -t v93k_RH4.rb -r approved)
-    load "#{Origen.top}/lib/origen/commands/generate.rb"
-    ARGV = %w(jtag_workout -t debug_RH4 -r approved)
-    load "#{Origen.top}/lib/origen/commands/generate.rb"
-    ARGV = %w(jtag_workout -t debug_RL4 -r approved)
-    load "#{Origen.top}/lib/origen/commands/generate.rb"
-    
-    if Origen.app.stats.changed_files == 0 &&
-       Origen.app.stats.new_files == 0 &&
-       Origen.app.stats.changed_patterns == 0 &&
-       Origen.app.stats.new_patterns == 0
+  ARGV = %w(jtag_workout -t v93k_RH4.rb -r approved)
+  load "#{Origen.top}/lib/origen/commands/generate.rb"
+  ARGV = %w(jtag_workout -t debug_RH4 -r approved)
+  load "#{Origen.top}/lib/origen/commands/generate.rb"
+  ARGV = %w(jtag_workout -t debug_RL4 -r approved)
+  load "#{Origen.top}/lib/origen/commands/generate.rb"
+  
+  if Origen.app.stats.changed_files == 0 &&
+     Origen.app.stats.new_files == 0 &&
+     Origen.app.stats.changed_patterns == 0 &&
+     Origen.app.stats.new_patterns == 0
 
-      Origen.app.stats.report_pass
-    else
-      Origen.app.stats.report_fail
-    end
-    puts ""
+    Origen.app.stats.report_pass
+  else
+    Origen.app.stats.report_fail
+    status = 1
   end
-  exit 0
+  puts
+  exit status
 
 # Always leave an else clause to allow control to fall back through to the
 # Origen command handler.
